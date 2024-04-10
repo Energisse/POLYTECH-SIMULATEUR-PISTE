b993367bd2db7be1f89963c3599fa70fe35ef4dd
florent <florent.moulon@gmail.com>
2024-04-10 14:16:20 +0200

    add Logiparam

---
+++ src/main/java/com/epul/demo/domain/LogiParam.java
@@ -0,0 +1,28 @@
+package com.epul.demo.domain;
+
+public class LogiParam {
+    private String nomUtil;
+    private String motPasse;
+
+    public String getMotPasse() {
+        return motPasse;
+    }
+
+    public void setNomUtil(String nomUtil) {
+        this.nomUtil = nomUtil;
+    }
+
+    public void setMotPasse(String motPasse) {
+        this.motPasse = motPasse;
+    }
+
+
+    public String getNomUtil() {
+        return nomUtil;
+    }
+
+}
+
+
+
+
